eef913211c506de601e2c4d700bd6b4cd245ef35
Sergryap <rs1180@mail.ru>
2023-03-09 21:06:02 +0500

    The not existing catalogue verify added

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ from aiohttp import web
 import aiofiles
 import asyncio
 import os
+from textwrap import dedent
 from asyncio.subprocess import create_subprocess_shell
 
 
@@ -13,23 +14,33 @@ async def archive(request):
     response = web.StreamResponse()
     original_folder = request.match_info.get('archive_hash')
     archive_path = os.path.join(os.getcwd(), 'test_photos', original_folder)
-    response.headers['Content-Disposition'] = f'attachment; filename="archive_{original_folder}.zip"'
-    response.headers['Content-Type'] = 'multipart/form-data'
-    await response.prepare(request)
-    cmd = f'zip -r - {" ".join(os.listdir(archive_path))}'
-    process = await create_subprocess_shell(
-        cmd,
-        stdout=asyncio.subprocess.PIPE,
-        stderr=asyncio.subprocess.PIPE,
-        cwd=archive_path
-    )
-    byte = SIZE_KB * BYTES_IN_KB
-    while True:
-        stdout = await process.stdout.read(byte)
-        await response.write(stdout)
-        if process.stdout.at_eof():
-            await response.write_eof(stdout)
-            break
+    if os.path.isdir(archive_path) and os.listdir(archive_path):
+        response.headers['Content-Disposition'] = f'attachment; filename="archive_{original_folder}.zip"'
+        response.headers['Content-Type'] = 'multipart/form-data'
+        await response.prepare(request)
+        cmd = f'zip -r - {" ".join(os.listdir(archive_path))}'
+        process = await create_subprocess_shell(
+            cmd,
+            stdout=asyncio.subprocess.PIPE,
+            stderr=asyncio.subprocess.PIPE,
+            cwd=archive_path
+        )
+        byte = SIZE_KB * BYTES_IN_KB
+        while True:
+            stdout = await process.stdout.read(byte)
+            await response.write(stdout)
+            if process.stdout.at_eof():
+                await response.write_eof(stdout)
+                break
+    else:
+        raise web.HTTPNotFound(
+            text=dedent(
+                '''
+                404 - страница не найдена.
+                Архив не существует или был удален
+                '''
+                )
+            )
 
 
 async def handle_index_page(request):
