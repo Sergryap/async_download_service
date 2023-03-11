da8898d4b5f9f298d24f7e249bf22aa269945afd
Sergryap <rs1180@mail.ru>
2023-03-11 22:16:39 +0500

    Added argparse

--- server.py
+++ server.py
@@ -2,32 +2,39 @@ import logging
 import aiofiles
 import asyncio
 import os
+import argparse
 
 from textwrap import dedent
-from asyncio.subprocess import create_subprocess_shell, create_subprocess_exec
+from asyncio.subprocess import create_subprocess_exec
 from aiohttp import web
 
-logging.basicConfig(
-    format=u'%(filename)s[LINE:%(lineno)d]# %(levelname)-5s [%(asctime)s]  %(message)s',
-    level=logging.DEBUG
-)
-logger = logging.getLogger('downloader')
+
+parser = argparse.ArgumentParser(prog='DownloadService', description='Сервис для скачивания файлов')
+parser.add_argument('-l', '--logging', required=False, action='store_true', help='Включение логирования')
+parser.add_argument('-dl', '--delay', required=False, default=0, type=int, help='Задержка ответа при скачивании, сек')
+parser.add_argument('-p', '--path', required=False, help='Полный путь к каталогу с файлами')
+args = parser.parse_args()
+
+if args.logging:
+    logging.basicConfig(
+        format=u'%(filename)s[LINE:%(lineno)d]# %(levelname)-5s [%(asctime)s]  %(message)s',
+        level=logging.DEBUG
+    )
+    logger = logging.getLogger('downloader')
 
 
 SIZE_KB = 100
 BYTES_IN_KB = 1024
 
 
-async def finally_handler():
-    res = input("Ctrl-c was pressed. Do you really want to exit? y/n ")
-    if res == 'y':
-        exit(1)
-
-
 async def archive(request):
     response = web.StreamResponse()
-    original_folder = request.match_info.get('archive_hash')
-    archive_path = os.path.join(os.getcwd(), 'test_photos', original_folder)
+    if args.path:
+        original_folder = os.path.split(args.path)[1]
+        archive_path = args.path
+    else:
+        original_folder = request.match_info.get('archive_hash')
+        archive_path = os.path.join(os.getcwd(), 'test_photos', original_folder)
     if os.path.isdir(archive_path) and os.listdir(archive_path):
         response.headers['Content-Disposition'] = f'attachment; filename="archive_{original_folder}.zip"'
         response.headers['Content-Type'] = 'multipart/form-data'
@@ -42,8 +49,10 @@ async def archive(request):
         byte = SIZE_KB * BYTES_IN_KB
         try:
             while True:
-                logger.info('Sending archive chunk ...')
-                await asyncio.sleep(1)
+                if args.logging:
+                    logger.info('Sending archive chunk ...')
+                if args.delay:
+                    await asyncio.sleep(args.delay)
                 stdout = await process.stdout.read(byte)
                 await response.write(stdout)
                 if process.stdout.at_eof():
@@ -53,7 +62,8 @@ async def archive(request):
             process.terminate()
             await process.communicate()
         except asyncio.CancelledError:
-            logger.warning('Download was interrupted')
+            if args.logging:
+                logger.warning('Download was interrupted')
             process.terminate()
             await process.communicate()
             raise
